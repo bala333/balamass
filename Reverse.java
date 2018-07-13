1e7e3e60d178c8c510e159b59c1533414ae598b1
bala333 <bala461997@gmail.com>
2018-07-13 22:41:12 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Playerguvi011
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        StringBuffer sb=new StringBuffer(s);
+        String s1="";
+        s1=String.valueOf(sb.reverse());
+        System.out.print(s1);
+    }
+}
